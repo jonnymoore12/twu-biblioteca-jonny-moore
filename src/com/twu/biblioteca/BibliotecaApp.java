d9be77c1e5e3a8b8b06b79078f8cb424ff8fbd8d
Jonny Moore <jonnymoore121212@gmail.com>
2017-02-20 12:34:29 +0000

    adds Book class and getter methods

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,12 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        bibliotecaApp.run();
     }
+
+    public void run() {
+        System.out.println("Welcome to Biblioteca!");
+    }
+
 }
